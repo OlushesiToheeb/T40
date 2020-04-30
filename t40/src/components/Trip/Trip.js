b32c4d7e336245b42c3ba7165d5768fa83ffd647
Adebayo-Ige <adebayoige.toyosi@gmail.com>
2020-04-30 18:14:09 +0100

    toheeb olodo

--- t40/src/components/Trip/Trip.js
+++ t40/src/components/Trip/Trip.js
@@ -1,147 +1,130 @@
 import React from 'react';
-import './Trip.css';
-import bg from '../../assets/imgs/hero.jpg';
-
 import axios from 'axios';
+
 import Form from '../Form/Form';
+import bg from '../../assets/imgs/hero.jpg';
+import './Trip.css';
 
+class Trip extends React.Component {
+	state = {
+		searchResult: null,
+		loading: true,
+		inputValues: null,
+	}
 
-
-class Trip extends React.Component{
-   
-    state={
-        searchResult:null,
-				loading:true,
-				inputValues: null
-    }
-
-    searchResultHandler = () =>{
-        axios.get('https://react-t40-ced15.firebaseio.com/bookRide.json')
-            .then(res=>{
-                console.log(res,res.data)
-                this.setState({searchResult : res.data, loading:false})
-                console.log(this.state.searchResult)
-
-            })
-            .catch(err => {
-                console.log(err.message)
-            })
-		}
-
-		childToParentHandler = (data) =>{
-			Object.entries(data).forEach(([key, value])=>{
-				console.log(key, value)
-				return {key :value }
+	searchResultHandler = () => {
+		axios.get('https://react-t40-ced15.firebaseio.com/bookRide.json')
+			.then(res => {
+				console.log(res, res.data)
+				this.setState({ searchResult: res.data, loading: false })
+				console.log(this.state.searchResult)
 			})
+			.catch(err => {
+				console.log(err.message)
+			})
+	}
 
-			console.log(data)
-		}
-		
-		 UI = () =>{
+	childToParentHandler = (data) => {
+		this.setState({inputValues: data})
+	}
 
-				return Object.keys(this.state.searchResult).map( result => {
-					console.log(this.state.searchResult[result])
-					return(
-							<div className='task-listing-container'>
-								<div className='task-listing'>
-										<div className='task-listing-details'>
-												<div className='task-listing-description'>
-														<div className='task-tag' style={{marginTop: '15px'}}>
-																<span> From </span>
-														</div>
-														<h3 className='task-listing-title'>Abuja-JabiPark (T40)</h3>
-														<ul className='task-icon'>
-																<li>{this.state.searchResult[result].fro.time}</li>
-																<li>Monday, April 27th 2020</li>
-														</ul>
-												</div>
+	UI = () => {
 
-												<div className='task-listing-description'>
-														<div className='task-tag' style={{marginTop: '15px'}}>
-																<span> To </span>
-														</div>
-														<h3 className='task-listing-title'>Akure (T40)</h3>
-														<ul className='task-icon'>
-																<li>{this.state.searchResult[result].to.time}</li>
-																<li> {this.state.startDate} </li>
-														</ul>
-												</div>
-										</div>
-										<div className='task-listing-bid'>
-												<div className='task-listing-bid-inner'>
-														<div className='task-offer'>
-																<strong>₦{this.state.searchResult[result].price} per passenger</strong>
-														</div>
-														<span className='button'>Book Now</span>
-												</div>
-										</div>
+		return Object.keys(this.state.searchResult).map(result => {
+			console.log(this.state.searchResult[result])
+			return (
+				<div className='task-listing-container'>
+					<div className='task-listing'>
+						<div className='task-listing-details'>
+							<div className='task-listing-description'>
+								<div className='task-tag' style={{ marginTop: '15px' }}>
+									<span> From </span>
 								</div>
-
+								<h3 className='task-listing-title'>{this.state.inputValues.cityFro}(T40)</h3>
+								<ul className='task-icon'>
+									<li>{this.state.searchResult[result].fro.time}</li>
+									<li>{this.state.inputValues.startDate.toDateString()}</li>
+								</ul>
+							</div>
+							<div className='task-listing-description'>
+								<div className='task-tag' style={{ marginTop: '15px' }}>
+									<span> To </span>
+								</div>
+								<h3 className='task-listing-title'>{this.state.inputValues.cityTo}(T40)</h3>
+								<ul className='task-icon'>
+									<li>{this.state.searchResult[result].to.time}</li>
+									<li> {this.state.startDate} </li>
+								</ul>
+							</div>
 						</div>
-					)
-					
+						<div className='task-listing-bid'>
+							<div className='task-listing-bid-inner'>
+								<div className='task-offer'>
+									<strong>₦{this.state.searchResult[result].price} per passenger</strong>
+								</div>
+								<span className='button'>Book Now</span>
+							</div>
+						</div>
+					</div>
+				</div>
+			)
+		})
+	}
 
-			})
-		}
+	render() {
 
-   
+		return (
+			<React.Fragment>
+				<div className="Hero" style={{ backgroundImage: " linear-gradient(to right bottom, rgba(252, 84, 38, 0.5), rgba(252, 84, 38, 0.8)), url(" + bg + ")" }}>
+					<div className="container">
+						<div className="row">
+							<div className="col-md-12">
+								<div className="banner-Header">
+									<h3>
+										<strong style={{ color: " rgb(255, 255, 255)" }}>Find and book car and bus rides in Nigeria</strong><br />
+										<span style={{ color: "rgb(255, 255, 255)" }}>Search. Compare. Book. Track.</span>
+									</h3>
+								</div>
+							</div>
+						</div>
+						<div className='form-wrapper'>
+							<div className="container">
+								<Form
+									searchResultHandle={this.searchResultHandler}
+									callFromParent={this.childToParentHandler} />
+							</div>
+						</div>
 
+					</div>
 
-    render(){    
+				</div>
+				<section style={{ paddingBottom: "75px", paddingTop: '65px' }}>
+					<div className='container'>
+						<div className='row'>
+							<div className='col-xl-12'>
+								<div style={{ marginBottom: '35px', marginTop: ' 0', textAlign: "center" }}>
+									<h3>Search Results</h3>
+								</div>
+								{this.state.loading ? (<p
+									style={{
+										marginBottom: '35px',
+										marginTop: ' 0',
+										textAlign: "center",
+										fontSize: '16px',
+										color: '#666'
+									}}>
+									No available rides
+								</p>) : this.UI()}
+							</div>
+						</div>
+					</div>
+				</section>
+			</React.Fragment>
 
-        return(
-						
-            <React.Fragment>
-							
-                <div className="Hero" style={{backgroundImage: " linear-gradient(to right bottom, rgba(252, 84, 38, 0.5), rgba(252, 84, 38, 0.8)), url(" + bg + ")"}}>
-                    <div className="container">
-                        <div className="row">
-                            <div className="col-md-12">
-                                <div className = "banner-Header">
-                                    <h3>
-                                        <strong style={{color:" rgb(255, 255, 255)"}}>Find and book car and bus rides in Nigeria</strong><br/>
-                                        <span style={{color: "rgb(255, 255, 255)"}}>Search. Compare. Book. Track.</span>
-                                    </h3>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className='form-wrapper'>
-                            <div className="container">
-																<Form 
-																	searchResultHandle={this.searchResultHandler}
-																	callFromParent={this.childToParentHandler}/>
-                            </div>
-                        </div>
-
-                    </div>
-                    
-                </div>
-                <section style={{ paddingBottom: "75px", paddingTop: '65px'}}>
-                    <div className ='container'>
-                        <div className='row'>
-                            <div className='col-xl-12'>
-                                <div style={{marginBottom: '35px', marginTop:' 0', textAlign:"center"}}>
-                                    <h3>Search Results</h3>
-                                </div>   
-                                {this.state.loading ? (<p
-                                                        style={{marginBottom: '35px', 
-                                                        marginTop:' 0', 
-																												textAlign:"center",
-																												fontSize:'16px',
-																												color:'#666'}}>
-                                                            No available rides
-                                                    </p>) : this.UI()}  
-                            </div>
-                        </div>
-                    </div>
-                </section>
-          	</React.Fragment>
-        
-        )
+		)
 
 
-    }
+	}
 }
 
 export default Trip
