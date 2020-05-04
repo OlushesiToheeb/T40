df7feb832ee6afffe15a73e15b41bb23121ee92d
OlushesiToheeb <olaolushesi98@gmail.com>
2020-05-04 15:11:45 +0100

    :ocean: refactor page loader

--- t40/src/App.js
+++ t40/src/App.js
@@ -8,44 +8,17 @@ import Review from '../src/components/Review/Review'
 
 class APP extends React.Component {
 
-  state={
-    loadingUI:false
-  }
-
-  componentDidMount(){
-		this.onLoad().then(() => this.setState({loadingUI : true}))
-	}
-
-	onLoad = () => {
-		return new Promise((resolve) => setTimeout(() => resolve(), 1500));
-	}
-
   render(){
-    
-    let allUI = (
-      
-      <Layout>
-        <Switch>
-          <Route exact path='/' component={Trip} />
-          <Route exact path='/track' component={TrackRide} />
-          <Route exact path='/review' component={Review} />
-        </Switch>
-      </Layout>
-      
-    )
-
-    
 
     return (
       <React.Fragment>
-        
-        {this.state.loadingUI ? allUI : 
-        <div className='d-flex justify-content-center' style={{marginTop:'15rem'}}>
-          <div class="spinner-grow m-20" role="status" style={{color: '#FE3D00'}}>
-            <span class="sr-only">Loading...</span>
-          </div> 
-        </div>}
-        
+        <Layout>
+          <Switch>
+            <Route exact path='/' component={Trip} />
+            <Route exact path='/track' component={TrackRide} />
+            <Route exact path='/review' component={Review} />
+          </Switch>
+        </Layout>
       </React.Fragment>
     );
 
